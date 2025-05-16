c1016acc3195f9d73f2b9c6f5f75e2a21861e2f6
ttrampp <ttrampp@msn.com>
2025-05-16 08:05:41 -0600

    database name change

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340tt;
+    OWNER TO cse340tt2;
 
 -- Table structure for table `classification`
 CREATE TABLE public.classification (
