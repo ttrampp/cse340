12b4cbfb51ce9def254f031bfc9a8637a800b484
ttrampp <ttrampp@msn.com>
2025-05-22 07:41:06 -0600

    team activity debugging complete

--- controllers/invController.js
+++ controllers/invController.js
@@ -9,6 +9,7 @@ const invCont = {}
 invCont.buildByClassificationId = async function (req, res, next) {
   const classification_id = req.params.classificationId
   const data = await invModel.getInventoryByClassificationId(classification_id)
+
   const grid = await utilities.buildClassificationGrid(data)
   let nav = await utilities.getNav()
   const className = data[0].classification_name
